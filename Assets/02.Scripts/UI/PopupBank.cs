0de1f945d73cc87af8d43cc3679a4f7512cee723
Jujeongmin <anjshdkdl99@gmail.com>
2025-03-20 19:35:23 +0900

    Feat : 입출금 기능 구현

--- Assets/02.Scripts/UI/PopupBank.cs
+++ Assets/02.Scripts/UI/PopupBank.cs
@@ -1,6 +1,5 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PopupBank : MonoBehaviour
 {
@@ -9,10 +8,20 @@ public class PopupBank : MonoBehaviour
     public TextMeshProUGUI balanceText;
 
     public GameObject Buttons;
-        
+    public GameObject Deposit;
+    public GameObject Withdraw;
+    public GameObject lack;
+
+    public TMP_InputField depositInputField;
+    public TMP_InputField withdrawInputField;
+
     void Start()
     {
         Refresh();
+        Buttons.SetActive(true);
+        Deposit.SetActive(false);
+        Withdraw.SetActive(false);
+        lack.SetActive(false);
     }
 
     public void Refresh()
@@ -22,9 +31,101 @@ public class PopupBank : MonoBehaviour
         balanceText.text = GameManager.Instance.userData.balance.ToString("N0");
     }
 
-    public void OnClick()
+    public void DepositBtnOnClick()
     {
-        //입금 버튼을 클릭하면 입금화면 켜지고 입금출금버튼 꺼지고
-        //출근 버튼을 클릭하면 출금화면 켜지고 입금출금버튼 꺼지고
+        Deposit.SetActive(true);
+        Buttons.SetActive(false);
+    }
+
+    public void WithdrawBtnOnClick()
+    {
+        Withdraw.SetActive(true);
+        Buttons.SetActive(false);
+    }
+
+    public void BackBtnOnClick()
+    {
+        Deposit.SetActive(false);
+        Withdraw.SetActive(false);
+        Buttons.SetActive(true);
+    }
+
+    public void DepositCashOnClick(int value)
+    {
+        if (GameManager.Instance.userData.cash >= value)
+        {
+            GameManager.Instance.userData.cash -= value;
+            GameManager.Instance.userData.balance += value;
+            Refresh();
+        }
+        else
+        {
+            lack.SetActive(true);
+        }
+    }
+
+    public void CustomDepositBtnOnClick()
+    {
+        if (int.TryParse(depositInputField.text, out int value) && value > 0)
+        {
+            if (GameManager.Instance.userData.cash >= value)
+            {
+                GameManager.Instance.userData.cash -= value;
+                GameManager.Instance.userData.balance += value;
+                depositInputField.text = "";
+                Refresh();
+            }
+            else
+            {
+                depositInputField.text = "";
+                lack.SetActive(true);
+            }
+        }
+        else
+        {
+            depositInputField.text = "";
+        }
+    }
+
+    public void LackOKButtonOnClick()
+    {
+        lack.SetActive(false);
+    }
+
+    public void WithdrawCashOnClick(int value)
+    {
+        if (GameManager.Instance.userData.balance >= value)
+        {
+            GameManager.Instance.userData.cash += value;
+            GameManager.Instance.userData.balance -= value;
+            Refresh();
+        }
+        else
+        {
+            lack.SetActive(true);
+        }
+    }
+
+    public void CustomWithdrawBtnOnClick()
+    {
+        if (int.TryParse(withdrawInputField.text, out int value) && value > 0)
+        {
+            if (GameManager.Instance.userData.balance >= value)
+            {
+                GameManager.Instance.userData.cash += value;
+                GameManager.Instance.userData.balance -= value;
+                withdrawInputField.text = "";
+                Refresh();
+            }
+            else
+            {
+                withdrawInputField.text = "";
+                lack.SetActive(true);
+            }
+        }
+        else
+        {
+            withdrawInputField.text = "";
+        }
     }
 }
