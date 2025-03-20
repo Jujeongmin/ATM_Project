7366f06dd01e6b58e97c7c7b996ce5fc4e9f120b
Jujeongmin <anjshdkdl99@gmail.com>
2025-03-20 16:27:23 +0900

    Feat : 입출금 UI 작성

--- Assets/02.Scripts/UI/PopupBank.cs
+++ Assets/02.Scripts/UI/PopupBank.cs
@@ -1,11 +1,14 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PopupBank : MonoBehaviour
 {
     public TextMeshProUGUI nameText;
     public TextMeshProUGUI cashText;
     public TextMeshProUGUI balanceText;
+
+    public GameObject Buttons;
         
     void Start()
     {
@@ -17,5 +20,11 @@ public class PopupBank : MonoBehaviour
         nameText.text = GameManager.Instance.userData.name;
         cashText.text = GameManager.Instance.userData.cash.ToString("N0");
         balanceText.text = GameManager.Instance.userData.balance.ToString("N0");
+    }
+
+    public void OnClick()
+    {
+        //입금 버튼을 클릭하면 입금화면 켜지고 입금출금버튼 꺼지고
+        //출근 버튼을 클릭하면 출금화면 켜지고 입금출금버튼 꺼지고
     }
 }
